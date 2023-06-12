1e361584cfb6224c9107ea0684a97aeeafdede7b
Maria <mas04081997@mail.ru>
2023-06-12 18:16:25 +0300

    if/else

---
+++ Example005_if/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имф пользователя");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "masha")
+{   
+    Console.WriteLine("Ура, это же MASHA");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
